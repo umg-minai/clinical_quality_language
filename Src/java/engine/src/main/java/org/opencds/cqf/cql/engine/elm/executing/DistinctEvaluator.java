2e23e001e9c5ba8a63d7d4c3cf8f415d8dc1a990
Jan Moringen <jan.moringen@med.uni-greifswald.de>
2026-06-10 15:31:27 +0200

    Use canUseEquals in DistinctEvaluator

--- Src/java/engine/src/main/java/org/opencds/cqf/cql/engine/elm/executing/DistinctEvaluator.java
+++ Src/java/engine/src/main/java/org/opencds/cqf/cql/engine/elm/executing/DistinctEvaluator.java
@@ -1,39 +1,52 @@
 package org.opencds.cqf.cql.engine.elm.executing;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import org.opencds.cqf.cql.engine.execution.CqlEngine;
 import org.opencds.cqf.cql.engine.execution.State;
 
-/*
-distinct(argument List<T>) List<T>
-
-The distinct operator returns the given list with duplicates eliminated using equality semantics.
-
-If the argument is null, the result is null.
-*/
-
+/**
+ * {@code distinct(argument List<T>) List<T>}
+ * <br/>
+ * The distinct operator returns the given list with duplicates eliminated using equality semantics.
+ * <br/>
+ * If the argument is null, the result is null.
+ */
 public class DistinctEvaluator {
 
-    public static List<Object> distinct(Iterable<?> source, State state) {
+    public static List<Object> distinct(final Iterable<?> source, final State state) {
         if (source == null) {
             return null;
         }
-
-        List<Object> result = new ArrayList<>();
+        final var hedisMode = state.getEngineOptions().contains(CqlEngine.Options.EnableHedisCompatibilityMode);
+        final var seen = new IdentityHashMap<Object, Boolean>();
+        final var distinctViaEquals = new HashSet<>();
+        final var result = new ArrayList<>();
         for (Object element : source) {
-            if (element == null && result.parallelStream().noneMatch(Objects::isNull)) {
-                result.add(null);
+            // Check whether element itself is already in the result.  This works correctly if element is null.
+            if (seen.containsKey(element)) {
                 continue;
             }
-
-            Object in = InEvaluator.in(element, result, null, state);
-
-            if (in == null) continue;
-
-            if (!(Boolean) in) result.add(element);
+            // If element itself is not in the result, we can mark it as "seen" since either it will be added now or
+            // there is an element in the result that is equal to element. In either case, if we see element itself
+            // again, we can skip it.
+            seen.put(element, Boolean.TRUE);
+            // TODO: explain
+            //
+            // Hedis compatibility mode make the InEvaluator use equivalent instead of equal so don't use Object#equals
+            // when in Hedis compatibility mode.
+            if (element == null || (!hedisMode && state.canUseEquals(element))) {
+                if (!distinctViaEquals.contains(element)) {
+                    distinctViaEquals.add(element);
+                    result.add(element);
+                }
+            } else {
+                // Look for an element in result that is equal to element and add element if there is none.
+                final var in = InEvaluator.in(element, result, null, state);
+                if (in != null && !in) {
+                    result.add(element);
+                }
+            }
         }
-
         return result;
     }
 }
